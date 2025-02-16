6930647b1891a56ce732a93f88a222ff37603dd4
sihabutheen8117 <sihabutheen8117@gmail.com>
2025-02-16 11:29:52 +0530

    initial

---
+++ frontend/restaurant/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Teams : </h1>
+      <h3>Sihabutheen Haq</h3>
+      <h3>Mohamed Azeem</h3>
+      <h3>Mohamed Kader Ismail</h3>
+      <h3>Mohamed Ibrahim</h3>
+      <h3>Azan Kuthoos</h3>
+      <h3>Javith</h3>
+    </>
+  );
+}
+
+export default App;

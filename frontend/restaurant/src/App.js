2eb0aa5ce01a5c7d6721f05c7fded11f773ae64e
sihabutheen8117 <sihabutheen8117@gmail.com>
2025-03-02 20:32:14 +0530

    changes

--- frontend/restaurant/src/App.js
+++ frontend/restaurant/src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Teams : </h1>
+      <h1>Team : </h1>
       <h3>Sihabutheen Haq</h3>
       <h3>Mohamed Azeem</h3>
       <h3>Mohamed Kader Ismail</h3>
       <h3>Mohamed Ibrahim</h3>
       <h3>Azan Kuthoos</h3>
-      <h3>Javith</h3>
     </>
   );
 }
